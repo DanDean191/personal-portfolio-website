d56779f7806e9bb991a9a63109d926dca0164e54
DanDean191 <daniel.dean191@hotmail.com>
2021-11-07 21:58:09 +0000

    Working navbar

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 // Mobile menu toggle
 const mobileMenuBtn = document.querySelector('#mobile-menu-btn');
-mobileMenuBtn.addEventListener('click', () => {
-    document.querySelector('.nav-menu').classList.toggle('mobile-menu-open')
-});
+const toggleNav = () => document.querySelector('.nav-menu').classList.toggle('mobile-menu-open');
+mobileMenuBtn.addEventListener('click', toggleNav);
+
 
 //Dark mode toggle
 const darkModeBtn = document.querySelector('#dark-mode-btn');
@@ -11,3 +11,19 @@ darkModeBtn.addEventListener('click', () => {
     darkModeBtn.childNodes[3].classList.toggle('toggle-on');
 });
 
+
+// Calculates height of hero
+const heroHandler = e => {
+    const headerHeight = document.querySelector('header').scrollHeight;
+    const bodyHeight = e.currentTarget.window.innerHeight;
+    const hero = document.getElementById('hero');
+    hero.style.height = bodyHeight - headerHeight+2+"px";
+}
+
+window.addEventListener('load', heroHandler);
+window.addEventListener('resize', heroHandler);
+
+const dropdownArray = document.querySelector('.nav-menu').children[0].children;
+for(const item of dropdownArray) {
+    item.addEventListener('click', toggleNav);
+}
